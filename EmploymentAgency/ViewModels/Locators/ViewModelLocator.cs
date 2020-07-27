d7e1dc2e36e240f0d52e208ff42b851531f35220
Андрей Левченко <andrey.levchenko.2001@gmail.com>
2020-07-27 22:59:19 +0500

    Верстка страницы Main. Написание стилей.

--- EmploymentAgency/ViewModels/Locators/ViewModelLocator.cs
+++ EmploymentAgency/ViewModels/Locators/ViewModelLocator.cs
@@ -1,14 +1,24 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using EmploymentAgency.Services;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace EmploymentAgency.ViewModels.Locators
 {
     public class ViewModelLocator
     {
         private static ServiceProvider _provider;
-        
+
+        public MainWindowViewModel MainWindowViewModel => _provider.GetRequiredService<MainWindowViewModel>();
+
+        public MainViewModel MainViewModel => _provider.GetRequiredService<MainViewModel>();
+
         public static void Init()
         {
             var services = new ServiceCollection();
+
+            services.AddTransient<MainWindowViewModel>();
+            services.AddTransient<MainViewModel>();
+
+            services.AddSingleton<PageService>();
 
             _provider = services.BuildServiceProvider();
 
---
+++ EmploymentAgency/ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using DevExpress.Mvvm;
+using EmploymentAgency.Services;
+using System.Windows;
+using System.Windows.Input;
+
+namespace EmploymentAgency.ViewModels
+{
+    public class MainViewModel : BindableBase
+    {
+        private readonly PageService _pageService;
+
+        public MainViewModel(PageService pageService)
+        {
+            _pageService = pageService;
+        }
+
+        public ICommand Exit => new DelegateCommand(() =>
+        {
+            Application.Current.Shutdown();
+        });
+    }
+}
